1276b7ca2a1a110d3d1966f0ff494465723a41d1
Midian-Tech <150345987+Midian-Tech@users.noreply.github.com>
2023-11-09 15:53:09 +0300

    Update my.js

--- my.js
+++ my.js
@@ -1,11 +1,33 @@
-<script>
-         function myfun(){
-    var A=document.getElementById("input1").value
+// Function to handle navigation
+function navigateTo(page) {
+  window.location.href = page + ".html";
+}
 
-    var B=document.getElementById("input2").value
-     
-    var mess="hello "+A+" your password is "+B
+// Check if the user is logged in (you should implement your login logic)
+function isUserLoggedIn() {
+  // Replace this with your actual login check
+  return false;
+}
 
-    document.getElementById("output").innerHTML=mess
- }
-    </script>
+// Event listener for login form
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+  if (isUserLoggedIn()) {
+    // Redirect to the "About" page after login
+    navigateTo("about");
+  } else {
+    alert("Login failed. Please check your credentials.");
+  }
+});
+
+// Event listeners for navigation links
+document.getElementById("about-link").addEventListener("click", function (e) {
+  e.preventDefault();
+  navigateTo("about");
+});
+
+document.getElementById("services-link").addEventListener("click", function (e) {
+  e.preventDefault();
+  navigateTo("services");
+});
