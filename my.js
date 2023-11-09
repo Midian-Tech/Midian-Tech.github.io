6160f753153bd3f4af2a62e136ebae62d7cabb8b
Midian-Tech <150345987+Midian-Tech@users.noreply.github.com>
2023-11-09 15:19:29 +0300

    Add files via upload

---
+++ my.js
@@ -0,0 +1,11 @@
+<script>
+         function myfun(){
+    var A=document.getElementById("input1").value
+
+    var B=document.getElementById("input2").value
+     
+    var mess="hello "+A+" your password is "+B
+
+    document.getElementById("output").innerHTML=mess
+ }
+    </script>
